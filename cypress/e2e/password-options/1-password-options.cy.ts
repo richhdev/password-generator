17f1584327299d304ac3da0efa981ac14a80b48a
richh <dev@richh.io>
2023-08-14 19:08:06 +0200

    try add empty export to fix pipeline issue

--- cypress/e2e/password-options/1-password-options.cy.ts
+++ cypress/e2e/password-options/1-password-options.cy.ts
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+export {};
+
 const regexLowercaseOnly = /^[a-z]+$/g;
 const regexUppercaseOnly = /^[A-Z]+$/g;
 const regexNumbersOnly = /^[0-9]+$/g;
@@ -170,5 +172,3 @@ describe("generates-password", () => {
     cy.get("#password").invoke("text").should("match", regexSpecialOnly);
   });
 });
-
-export {};
